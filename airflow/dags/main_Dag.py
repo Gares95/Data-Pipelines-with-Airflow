710f99e5a86050b8ec733b5d10588a9640f8050c
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-10-12 21:19:37 +0200

    Completed stage_redshift operator and added to main_dag with variables

--- airflow/dags/main_Dag.py
+++ airflow/dags/main_Dag.py
@@ -33,12 +33,26 @@ start_operator = PostgresOperator(
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    region = "us-west-2",
+    extra_params = "FORMAT AS JSON 's3://udacity-dend/log_json_path.json'"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table="staging_songs",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    region = "us-west-2",
+    extra_params="JSON 'auto'"
 )
 
 load_songplays_table = LoadFactOperator(
