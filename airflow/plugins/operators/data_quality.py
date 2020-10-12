cf670284154c5ad1a6dbeaed15b7e9c252e0d579
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-10-12 21:24:42 +0200

    Completed data_quality operator and added to main dag with variables

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -1,3 +1,4 @@
+import logging
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -8,15 +9,29 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 tables=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('DataQualityOperator work in progress')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        
+        self.log.info('This are my tables {}'.format(self.tables))
+        for table in self.tables: 
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            self.log.info("This records are of type: {}".format(type(records)))
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+            if any(elem is None for elem in records):
+                raise ValueError(f"Data quality check failed. {table} contains None element(s)")
+            logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+
+            
